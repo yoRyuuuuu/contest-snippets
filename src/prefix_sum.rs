4390f8380d05b583e179f0cdd309e73bc388af48
yoRyuuuuu <sasoseooo@gmail.com>
2021-06-06 07:12:11 +0900

    PrefixSum2Dを修正

--- src/prefix_sum.rs
+++ src/prefix_sum.rs
@@ -1,22 +1,52 @@
-pub struct PrefixSum2D {
-    sum: Vec<Vec<i64>>,
-    vec: Vec<Vec<i64>>,
+use crate::number::Number;
+use cargo_snippet::snippet;
+#[snippet("PrefixSum2D")]
+use std::ops::{Add, Sub};
+
+#[snippet("PrefixSum2D")]
+pub struct PrefixSum2D<T> {
+    sum: Vec<Vec<T>>,
+    vec: Vec<Vec<T>>,
     h: usize,
     w: usize,
 }
 
-impl PrefixSum2D {
+#[snippet("PrefixSum2D")]
+impl<T> From<Vec<Vec<T>>> for PrefixSum2D<T>
+where
+    T: Number + Clone + Copy + Add<Output = T> + Sub<Output = T>,
+{
+    fn from(vec: Vec<Vec<T>>) -> Self {
+        let h = vec.len();
+        let w = vec[0].len();
+        let mut slf = Self {
+            sum: vec![vec![T::zero(); w + 1]; h + 1],
+            vec,
+            h,
+            w,
+        };
+        slf.build();
+
+        return slf;
+    }
+}
+
+#[snippet("PrefixSum2D")]
+impl<T> PrefixSum2D<T>
+where
+    T: Number + Clone + Copy + Add<Output = T> + Sub<Output = T>,
+{
     pub fn new(h: usize, w: usize) -> Self {
         Self {
-            sum: vec![vec![0i64; w + 1]; h + 1],
-            vec: vec![vec![0i64; w]; h],
+            sum: vec![vec![T::zero(); w + 1]; h + 1],
+            vec: vec![vec![T::zero(); w]; h],
             h,
             w,
         }
     }
 
-    pub fn add(&mut self, x: usize, y: usize, element: i64) {
-        self.vec[y][x] += element;
+    pub fn add(&mut self, x: usize, y: usize, element: T) {
+        self.vec[y][x] = self.vec[y][x] + element;
     }
 
     pub fn build(&mut self) {
@@ -28,7 +58,7 @@ impl PrefixSum2D {
         }
     }
 
-    pub fn query(&self, x1: usize, x2: usize, y1: usize, y2: usize) -> i64 {
+    pub fn query(&self, x1: usize, x2: usize, y1: usize, y2: usize) -> T {
         self.sum[y2][x2] - self.sum[y1][x2] - self.sum[y2][x1] + self.sum[y1][x1]
     }
 }
@@ -38,7 +68,6 @@ mod tests {
     use super::PrefixSum2D;
     #[test]
     fn test_prefix_sum_2d() {
-        let mut r = PrefixSum2D::new(5, 5);
         let vec = vec![
             vec![1, 2, 3, 4, 5],
             vec![1, 2, 3, 4, 5],
@@ -46,6 +75,9 @@ mod tests {
             vec![1, 2, 3, 4, 5],
             vec![1, 2, 3, 4, 5],
         ];
+
+        let mut r1 = PrefixSum2D::new(5, 5);
+        let r2 = PrefixSum2D::from(vec.clone());
 
         let f = |x1: usize, x2: usize, y1: usize, y2: usize, vec: &Vec<Vec<i64>>| {
             let mut res = 0;
@@ -60,16 +92,17 @@ mod tests {
 
         for y in 0..5 {
             for x in 0..5 {
-                r.add(x, y, vec[y][x]);
+                r1.add(x, y, vec[y][x]);
             }
         }
-        r.build();
+        r1.build();
 
         for y1 in 0..5 {
             for y2 in y1 + 1..=5 {
                 for x1 in 0..5 {
                     for x2 in x1 + 1..=5 {
-                        assert_eq!(r.query(x1, x2, y1, y2), f(x1, x2, y1, y2, &vec));
+                        assert_eq!(r1.query(x1, x2, y1, y2), f(x1, x2, y1, y2, &vec));
+                        assert_eq!(r2.query(x1, x2, y1, y2), f(x1, x2, y1, y2, &vec));
                     }
                 }
             }
